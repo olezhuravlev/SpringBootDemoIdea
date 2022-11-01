026cd5afb59b86ea98bd0584efb24295d576a3b5
Oleg Zhuravlev <olezhuravlev@gmail.com>
2022-11-01 07:27:49 +0100

    Init.

---
+++ src/main/java/com/example/springbootdemoidea/SpringBootDemoIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemoidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoIdeaApplication.class, args);
+    }
+
+}
